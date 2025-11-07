019ecf6feefec30fadaaa0fcaf3951fc5985e000
Lokesh0018 <munakalalokesh222@gmail.com>
2025-11-07 11:17:34 +0530

    Designed Hero Navigation

---
+++ src/Hero/Hero.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { ReactComponent as MailIcon } from "../asserts/mail.svg";
+
+const Hero = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="hero">
+      <div className="heroHeader">
+        <nav className="nav">
+          <div className="navItems">
+            <ul className={isOpen ? "open" : ""}>
+              <li>Services</li>
+              <li>Their Stories</li>
+              <li>Our Story</li>
+              <li>Varnan</li>
+              <li>
+                <button className="cntBtn">
+                  Let’s Talk
+                  <span className="boundingBox">
+                    <MailIcon className="mailIcon" />
+                  </span>
+                </button>
+              </li>
+            </ul>
+
+            <div className="menu" onClick={toggleMenu}>
+              <div className={`bar top ${isOpen ? "open" : ""}`}></div>
+              <div className={`bar bottom ${isOpen ? "open" : ""}`}></div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
